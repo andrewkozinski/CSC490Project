12aa2ff4a1318de5ba909909ad07f1374e77e732
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-09-19 20:49:07 -0400

    Removed example dict and example get all movies route

--- backend/routes/movies.py
+++ backend/routes/movies.py
@@ -9,18 +9,6 @@ router = APIRouter()
 load_dotenv()
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 
-# Dictionary to simulate a database or API fetching movies
-movies = {
-        1: {"name": "Spiderverse 3", "director": "Never Coming Out", "year": 2099},
-        2: {"name": "CSC 343: David Gerstl's Wild Ride", "director": "David Gerstl", "year": 2025},
-        3: {"name": "Superman 2", "director": "James Gunn", "year": 2027}
-    }
-
-@router.get("/all")
-async def get_all_movies():
-    return movies
-
-
 @router.get("/search")
 async def search_movies(query: str, page: int = 1):
     url = f"https://api.themoviedb.org/3/search/movie?api_key={TMDB_API_KEY}&query={query}&page={page}"
