b4398d304e8cefe58c215707468799cc53e26354
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-09-19 22:49:24 -0400

    Changed director to created_by

--- backend/models/tvshow.py
+++ backend/models/tvshow.py
@@ -1,10 +1,11 @@
+from typing import List
 from pydantic import BaseModel
 
 
 class TvShow(BaseModel):
     id: str
     title: str
-    director: str
+    created_by: List[str]
     release_date: str
     seasons: int
     episodes: int
