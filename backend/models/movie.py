cb98553be8f8ce90fbcbf7d64e85158e29b62c85
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-09-20 15:20:44 -0400

    Added genre list as a member of Movie class

--- backend/models/movie.py
+++ backend/models/movie.py
@@ -1,8 +1,10 @@
+from typing import List
 from pydantic import BaseModel
 
 class Movie(BaseModel):
     id: str
     title: str
+    genre: List[str]
     director: str
     year: str
     release_date: str
