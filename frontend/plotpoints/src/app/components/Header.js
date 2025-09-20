a4cfc03ab954b47f0475f33d6fef155cc50c5bd5
Kathleen Tan <tank@farmingdale.edu>
2025-09-19 21:57:55 -0400

    Capitalized PLOT POINTS

--- frontend/plotpoints/src/app/components/Header.js
+++ frontend/plotpoints/src/app/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="mx-auto flex max-w-sm items-end gap-20">
       {/* <img src="images/plotpointslogo.png" alt="Plot Points Text" width={100} height={100}/> */}
-      <a className="inria-serif-regular text-4xl inline-block text-center" href="./">Plot Points</a>
+      <a className="inria-serif-regular text-4xl inline-block text-center" href="./">PLOT POINTS</a>
       <a className="headingStyling" href="/movies">Movies</a>
       <a className="headingStyling whitespace-nowrap" href="/tv">TV Shows</a>
       <a className="headingStyling" href="/books">Books</a>
