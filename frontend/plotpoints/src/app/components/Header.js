b64e4bd2d863584a9643d9c8b05776ee449d4378
[serrvl@farmingdale.edu] <serrvl@farmingdale.edu>
2025-09-21 16:51:31 -0400

    Moved the header over to the left side

--- frontend/plotpoints/src/app/components/Header.js
+++ frontend/plotpoints/src/app/components/Header.js
@@ -2,7 +2,7 @@ import "./Header.css";
 
 export default function Header() {
   return (
-    <header className="mx-auto flex max-w-sm items-end gap-20">
+    <header className="mx-5 flex max-w-sm items-end gap-20">
       {/* <img src="images/plotpointslogo.png" alt="Plot Points Text" width={100} height={100}/> */}
       <a className="inria-serif-regular text-4xl inline-block text-center" href="./">Plot Points</a>
       <a className="headingStyling" href="/movies">Movies</a>
