7d8f20516fcde10795a28008333474de59cbb1bc
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-09-23 20:13:53 -0400

    Added images based on sign in status

--- frontend/plotpoints/src/app/test/page.js
+++ frontend/plotpoints/src/app/test/page.js
@@ -2,17 +2,29 @@
 import { useSession } from "next-auth/react";
 import Header from "../components/Header";
 
+import Image from "next/image";
 export default function SessionInfo() {
   const { data: session, status } = useSession();
 
   if (status === "loading") return <p>Loading...</p>;
-  if (!session) return <p>No session found</p>;
+  if (!session) return (
+    <>
+      <p>No session found</p>
+      <Image src="/images/sad-tenna.gif" alt="No session" width={500} height={300} />
+    </>
+  );
 
   return (
     <div>
       <Header/>
       <h3>Session Data:</h3>
       <pre>{JSON.stringify(session, null, 2)}</pre>
+      <Image 
+        src="/images/spr_tenna_dance_cane_big.gif" 
+        alt="Description of my image" 
+        width={500} 
+        height={300} 
+      />
     </div>
   );
 }
