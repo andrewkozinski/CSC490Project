d34f337984e0aa0a1f85e11837342fe2b7eb9723
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-09-23 20:21:28 -0400

    Fixed header not appearing in loading state

--- frontend/plotpoints/src/app/test/page.js
+++ frontend/plotpoints/src/app/test/page.js
@@ -6,7 +6,13 @@ import Image from "next/image";
 export default function SessionInfo() {
   const { data: session, status } = useSession();
 
-  if (status === "loading") return <p>Loading...</p>;
+  if (status === "loading") return (
+    <>
+      <Header/>
+      <p>Loading...</p>
+      <Image src="/images/spr_tenna_t_pose_big.gif" alt="Loading" width={500} height={300} />
+    </>
+  );
   if (!session) return (
     <>
       <p>No session found</p>
