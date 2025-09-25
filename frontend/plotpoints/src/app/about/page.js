3794d337c39354b5fc1f80602cab2b7760820e20
Kathleen Tan <tank@farmingdale.edu>
2025-09-25 17:25:11 -0400

    Pages for About and Contact Us added

---
+++ frontend/plotpoints/src/app/about/page.js
@@ -0,0 +1,11 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+export default function About() {
+  return (
+    <div>
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
