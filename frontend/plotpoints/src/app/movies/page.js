f9acd364f92b68ce6da293eb0c3bd0eeb410f281
[serrvl@farmingdale.edu] <serrvl@farmingdale.edu>
2025-09-27 17:37:34 -0400

    Filled movies/shows/books with dummy data

--- frontend/plotpoints/src/app/movies/page.js
+++ frontend/plotpoints/src/app/movies/page.js
@@ -1,11 +1,142 @@
-import Footer from "../components/Footer";
 import Header from "../components/Header";
+import GenreContainer from "../components/GenreContainer";
+import Footer from "../components/Footer";
 
-export default function Movies() {
+export default function Books() {
   return (
     <div>
-      <Header/>
-      <Footer/>
+      <Header />
+      <main className="p-6">
+        <GenreContainer label="Horror Movies">
+          <img
+            src="https://image.tmdb.org/t/p/w500/22AouvwlhlXbe3nrFcjzL24bvWH.jpg"
+            title="Kpop Demon Hunters"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/wPLysNDLffQLOVebZQCbXJEv6E6.jpg"
+            title="Superman 2025"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/cpf7vsRZ0MYRQcnLWteD5jK9ymT.jpg"
+            title="Weapons"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/wobVTa99eW0ht6c1rNNzLkazPtR.jpg"
+            title="The Long Walk"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/m1jFoahEbeQXtx4zArT2FKdbNIj.jpg"
+            title="One Battle After Another"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/sUsVimPdA1l162FvdBIlmKBlWHx.jpg"
+            title="Demon Slayer"
+            className="cover"
+          />
+        </GenreContainer>
+        <GenreContainer label="Drama Movies">
+          <img
+            src="https://image.tmdb.org/t/p/w500/22AouvwlhlXbe3nrFcjzL24bvWH.jpg"
+            title="Kpop Demon Hunters"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/wPLysNDLffQLOVebZQCbXJEv6E6.jpg"
+            title="Superman 2025"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/cpf7vsRZ0MYRQcnLWteD5jK9ymT.jpg"
+            title="Weapons"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/wobVTa99eW0ht6c1rNNzLkazPtR.jpg"
+            title="The Long Walk"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/m1jFoahEbeQXtx4zArT2FKdbNIj.jpg"
+            title="One Battle After Another"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/sUsVimPdA1l162FvdBIlmKBlWHx.jpg"
+            title="Demon Slayer"
+            className="cover"
+          />
+        </GenreContainer>
+        <GenreContainer label="Comedy Movies">
+          <img
+            src="https://image.tmdb.org/t/p/w500/22AouvwlhlXbe3nrFcjzL24bvWH.jpg"
+            title="Kpop Demon Hunters"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/wPLysNDLffQLOVebZQCbXJEv6E6.jpg"
+            title="Superman 2025"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/cpf7vsRZ0MYRQcnLWteD5jK9ymT.jpg"
+            title="Weapons"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/wobVTa99eW0ht6c1rNNzLkazPtR.jpg"
+            title="The Long Walk"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/m1jFoahEbeQXtx4zArT2FKdbNIj.jpg"
+            title="One Battle After Another"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/sUsVimPdA1l162FvdBIlmKBlWHx.jpg"
+            title="Demon Slayer"
+            className="cover"
+          />
+        </GenreContainer>
+        <GenreContainer label="Science Fiction Movies">
+          <img
+            src="https://image.tmdb.org/t/p/w500/22AouvwlhlXbe3nrFcjzL24bvWH.jpg"
+            title="Kpop Demon Hunters"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/wPLysNDLffQLOVebZQCbXJEv6E6.jpg"
+            title="Superman 2025"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/cpf7vsRZ0MYRQcnLWteD5jK9ymT.jpg"
+            title="Weapons"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/wobVTa99eW0ht6c1rNNzLkazPtR.jpg"
+            title="The Long Walk"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/m1jFoahEbeQXtx4zArT2FKdbNIj.jpg"
+            title="One Battle After Another"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/sUsVimPdA1l162FvdBIlmKBlWHx.jpg"
+            title="Demon Slayer"
+            className="cover"
+          />
+        </GenreContainer>
+      </main>
+      <Footer />
     </div>
   );
 }
--- frontend/plotpoints/src/app/tv/page.js
+++ frontend/plotpoints/src/app/tv/page.js
@@ -1,11 +1,142 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import GenreContainer from "../components/GenreContainer";
 
 export default function TV() {
   return (
     <div>
-      <Header/>
-      <Footer/>
+      <Header />
+      <main className="p-6">
+        <GenreContainer label="Kids Shows">
+          <img
+            src="https://image.tmdb.org/t/p/w500/22AouvwlhlXbe3nrFcjzL24bvWH.jpg"
+            title="Kpop Demon Hunters"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/wPLysNDLffQLOVebZQCbXJEv6E6.jpg"
+            title="Superman 2025"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/cpf7vsRZ0MYRQcnLWteD5jK9ymT.jpg"
+            title="Weapons"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/wobVTa99eW0ht6c1rNNzLkazPtR.jpg"
+            title="The Long Walk"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/m1jFoahEbeQXtx4zArT2FKdbNIj.jpg"
+            title="One Battle After Another"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/sUsVimPdA1l162FvdBIlmKBlWHx.jpg"
+            title="Demon Slayer"
+            className="cover"
+          />
+        </GenreContainer>
+        <GenreContainer label="Drama Shows">
+          <img
+            src="https://image.tmdb.org/t/p/w500/22AouvwlhlXbe3nrFcjzL24bvWH.jpg"
+            title="Kpop Demon Hunters"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/wPLysNDLffQLOVebZQCbXJEv6E6.jpg"
+            title="Superman 2025"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/cpf7vsRZ0MYRQcnLWteD5jK9ymT.jpg"
+            title="Weapons"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/wobVTa99eW0ht6c1rNNzLkazPtR.jpg"
+            title="The Long Walk"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/m1jFoahEbeQXtx4zArT2FKdbNIj.jpg"
+            title="One Battle After Another"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/sUsVimPdA1l162FvdBIlmKBlWHx.jpg"
+            title="Demon Slayer"
+            className="cover"
+          />
+        </GenreContainer>
+        <GenreContainer label="Comedy Shows">
+          <img
+            src="https://image.tmdb.org/t/p/w500/22AouvwlhlXbe3nrFcjzL24bvWH.jpg"
+            title="Kpop Demon Hunters"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/wPLysNDLffQLOVebZQCbXJEv6E6.jpg"
+            title="Superman 2025"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/cpf7vsRZ0MYRQcnLWteD5jK9ymT.jpg"
+            title="Weapons"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/wobVTa99eW0ht6c1rNNzLkazPtR.jpg"
+            title="The Long Walk"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/m1jFoahEbeQXtx4zArT2FKdbNIj.jpg"
+            title="One Battle After Another"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/sUsVimPdA1l162FvdBIlmKBlWHx.jpg"
+            title="Demon Slayer"
+            className="cover"
+          />
+        </GenreContainer>
+        <GenreContainer label="Horror Shows">
+          <img
+            src="https://image.tmdb.org/t/p/w500/22AouvwlhlXbe3nrFcjzL24bvWH.jpg"
+            title="Kpop Demon Hunters"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/wPLysNDLffQLOVebZQCbXJEv6E6.jpg"
+            title="Superman 2025"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/cpf7vsRZ0MYRQcnLWteD5jK9ymT.jpg"
+            title="Weapons"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/wobVTa99eW0ht6c1rNNzLkazPtR.jpg"
+            title="The Long Walk"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/m1jFoahEbeQXtx4zArT2FKdbNIj.jpg"
+            title="One Battle After Another"
+            className="cover"
+          />
+          <img
+            src="https://image.tmdb.org/t/p/w500/sUsVimPdA1l162FvdBIlmKBlWHx.jpg"
+            title="Demon Slayer"
+            className="cover"
+          />
+        </GenreContainer>
+      </main>
+      <Footer />
     </div>
   );
 }
