c05f0dd0f4e8ba8f61f9219c65c22f6dbe761bf9
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-09-27 11:51:29 -0400

    Created functions for getting reviews by media type and id and getting reviews by user id

--- backend/routes/reviews.py
+++ backend/routes/reviews.py
@@ -55,3 +55,18 @@ async def get_reviews_by_media_type(media_type: str):
         raise HTTPException(status_code=500, detail="Failed to fetch reviews. Please try again.")
     return {"reviews": reviews_by_type}
 
+#Get all reviews by a media type and id
+@router.get("/by_media/{media_type}/{media_id}")
+async def get_reviews_by_media(media_type: str, media_id: str):
+    reviews_by_media = reviews.get_reviews_by_media_id_and_type(media_type.lower(), media_id)
+    if reviews_by_media is None:
+        raise HTTPException(status_code=500, detail="Failed to fetch reviews. Please try again.")
+    return {"reviews": reviews_by_media}
+
+#Get all reviews by a user id
+@router.get("/by_user/{user_id}")
+async def get_reviews_by_user(user_id: int):
+    reviews_by_user = reviews.get_reviews_by_user_id(user_id)
+    if reviews_by_user is None:
+        raise HTTPException(status_code=500, detail="Failed to fetch reviews. Please try again.")
+    return {"reviews": reviews_by_user}
