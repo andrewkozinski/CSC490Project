fa35bbc42ada57e8013b5d4793864b39e7f612c8
[serrvl@farmingdale.edu] <serrvl@farmingdale.edu>
2025-09-27 17:38:18 -0400

    Added comment to genreContainer

--- frontend/plotpoints/src/app/components/GenreContainer.js
+++ frontend/plotpoints/src/app/components/GenreContainer.js
@@ -1,5 +1,6 @@
 import "../globals.css"
 export default function GenreContainer({ label, children }) {
+    //Will scroll if more media is added
   return (
     <section className="my-8">
       <h2 className="text-2xl font-bold mb-4">{label}</h2>
