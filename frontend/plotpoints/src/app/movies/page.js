c2d0b050414939c374596804c04d760cb1753dab
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-09-27 22:34:05 -0400

    Fixed typo in function being called "Books" to "Movies"

--- frontend/plotpoints/src/app/movies/page.js
+++ frontend/plotpoints/src/app/movies/page.js
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 import GenreContainer from "../components/GenreContainer";
 import Footer from "../components/Footer";
 
-export default function Books() {
+export default function Movies() {
   return (
     <div>
       <Header />
