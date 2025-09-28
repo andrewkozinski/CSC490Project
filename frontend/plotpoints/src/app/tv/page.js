554962d140a19c661995bf882fe5a43fbf5b6227
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-09-27 23:33:34 -0400

    Added functionality to fetch data from the backend

--- frontend/plotpoints/src/app/tv/page.js
+++ frontend/plotpoints/src/app/tv/page.js
@@ -1,8 +1,40 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import GenreContainer from "../components/GenreContainer";
 
 export default function TV() {
+
+  const [kidsShows, setKidsShows] = useState([]);
+  const [dramaShows, setDramaShows] = useState([]);
+  const [comedyShows, setComedyShows] = useState([]);
+  const [crimeShows, setCrimeShows] = useState([]);
+
+  //Handles fetching tv shows from the backend
+  useEffect(() => {
+    // Fetch shows for each genre
+    const fetchShows = async (genre, setShows) => {
+      try {
+        const res = await fetch(`/api/tv/genre/${genre}`);
+        if (!res.ok) throw new Error("Failed to fetch TV shows");
+        const data = await res.json();
+        setShows(data.results || []);
+        console.log("GENRE: " + genre);
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    // Fetch shows for each genre
+    fetchShows("kids", setKidsShows);
+    fetchShows("drama", setDramaShows);
+    fetchShows("comedy", setComedyShows);
+    fetchShows("crime", setCrimeShows);    
+  }, []);
+
   return (
     <div>
       <Header />
