44b07640d64af2c3f6464e0884505cd336a5391e
Kathleen Tan <tank@farmingdale.edu>
2025-10-01 22:00:45 -0400

    Updated homepage with container for content

--- frontend/plotpoints/src/app/components/GenreContainer.js
+++ frontend/plotpoints/src/app/components/GenreContainer.js
@@ -3,8 +3,8 @@ export default function GenreContainer({ label, children }) {
     //Will scroll if more media is added
   return (
     <section className="my-8">
-      <h2 className="text-2xl font-bold mb-4">{label}</h2>
-      <div className="flex gap-5 overflow-x-auto px-7 py-7">
+      <h2 className="text-2xl font-bold pl-10">{label}</h2>
+      <div className="flex gap-5 overflow-x-auto px-10 py-5">
         {children}
       </div>
     </section>
