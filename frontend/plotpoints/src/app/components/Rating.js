c9f9ef812f9cce3b3d3ffe910d47355e328ab9d0
[serrvl@farmingdale.edu] <serrvl@farmingdale.edu>
2025-10-04 07:16:08 -0400

    Created most of the movie page

--- frontend/plotpoints/src/app/components/Rating.js
+++ frontend/plotpoints/src/app/components/Rating.js
@@ -3,11 +3,11 @@ import Star from "./Star";
 export default function Rating() {
   return (
     //reverse in order to make stars fill up properly
-    <div className="flex flex-row-reverse justify-center p-10">
+    <div className="flex flex-row-reverse justify-center p-4">
       {[...Array(5)].map((_, i) => (
         <a
           key={i}
-          className="cursor-pointer peer peer-hover:fill-yellow-500 hover:fill-yellow-500 fill-yellow-200"
+          className="cursor-pointer peer peer-hover:fill-yellow-500 hover:fill-yellow-400 fill-transparent stroke-neutral-950"
         >
           <Star className="w-8 h-8" />
         </a>
