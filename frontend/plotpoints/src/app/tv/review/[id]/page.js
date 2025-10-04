f41e460d56178f6a5b7f5871e9632f93f1675785
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-04 11:57:38 -0400

    Fixed import for react

--- frontend/plotpoints/src/app/tv/review/[id]/page.js
+++ frontend/plotpoints/src/app/tv/review/[id]/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, use } from "react";
+import { useState, useEffect} from "react";
 import React from "react";
 import Footer from "@/app/components/Footer";
 import Header from "../../../components/Header";
