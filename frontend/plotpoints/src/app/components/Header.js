e33aabf096e40eead835f0ed4c5557c9f53ac47b
Kathleen Tan <tank@farmingdale.edu>
2025-10-07 21:31:32 -0400

    Changed text sizes in header

--- frontend/plotpoints/src/app/components/Header.js
+++ frontend/plotpoints/src/app/components/Header.js
@@ -9,14 +9,14 @@ export default function Header() {
     <header className="mt-2 pb-3 flex justify-between items-center border-b">
       <nav className="flex mx-5 max-w-sm items-end gap-20">
         {/* <img src="images/plotpointslogo.png" alt="Plot Points Text" width={100} height={100}/> */}
-        <Link className="inria-serif-regular text-4xl inline-block text-center" href="/">PLOT POINTS</Link>
+        <Link className="inria-serif-regular text-3xl inline-block text-center" href="/">PLOT POINTS</Link>
         <Link className="text hoverCat" href="/movies">Movies</Link>
         <Link className="text hoverCat whitespace-nowrap" href="/tv">TV Shows</Link>
         <Link className="text hoverCat" href="/books">Books</Link>
       </nav>
       <nav className ="grid grid-rows-2 mr-5 justify-end">
         <div>
-          <Link className="text fields brown shadow mr-10 mt-1 hover:bg-black" href="/signup">Sign Up</Link>
+          <Link className="text fields brown shadow mr-10 mt-1" href="/signup">Sign Up</Link>
           <Link className="text fields blue shadow" href="/signin">Sign In</Link>
         </div>
         <div className="search">
