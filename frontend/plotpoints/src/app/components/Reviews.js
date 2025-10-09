672deba195b4a9da7c907941b41fb4446576a3e5
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-08 20:41:02 -0400

    Fixed strokeWidth, strokeLinejoin and className using incorrect dom properties

--- frontend/plotpoints/src/app/components/Reviews.js
+++ frontend/plotpoints/src/app/components/Reviews.js
@@ -40,8 +40,8 @@ export default function Reviews() {
               class="size-6"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
               />
             </svg>
@@ -55,13 +55,13 @@ export default function Reviews() {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
-              class="size-6"
+              className="size-6"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
               />
             </svg>
