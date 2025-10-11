5785a5a68f0ba2a2fb2339f2c0409ef24a1ceda5
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-11 15:46:39 -0400

    Created get_username_by_id function for profile.py

--- backend/routes/profile.py
+++ backend/routes/profile.py
@@ -1,4 +1,12 @@
 from fastapi import APIRouter, HTTPException
+from database.users import get_by_id
 
 router = APIRouter()
 
+@router.get("/get_username_by_id")
+async def get_username_by_id(user_id: int):
+    user = get_by_id(user_id)
+    if user:
+        return {"username": user.username}
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
