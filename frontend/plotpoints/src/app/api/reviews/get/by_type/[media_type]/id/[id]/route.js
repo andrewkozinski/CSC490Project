5a6a52e152006ad8a8b56eae623b001071a1862f
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-11 16:01:27 -0400

    Updated route.js to include context for get review by media id and type requests

--- frontend/plotpoints/src/app/api/reviews/get/by_type/[media_type]/id/[id]/route.js
+++ frontend/plotpoints/src/app/api/reviews/get/by_type/[media_type]/id/[id]/route.js
@@ -1,9 +1,12 @@
-export async function GET(request, { params }) {
+export async function GET(request, context) {
+    const params = await context.params;
     const { media_type, id } = params;
+
+    console.log(`Fetching reviews for media_type: ${media_type}, id: ${id}`);
 
     const res = await fetch(`${process.env.API_URL}/reviews/by_media/${media_type}/${id}`);
     if (!res.ok) {
-        return new Response("Failed to fetch reviews", { status: 500 });
+        return new Response("Failed to fetch reviews womp womp", { status: 500 });
     }
     const data = await res.json();
     return new Response(JSON.stringify(data), { status: 200 });
