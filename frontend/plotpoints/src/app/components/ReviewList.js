0fdaff14391e20d8e16b058cd60d2e9c4bbbc077
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-11 16:16:31 -0400

    Added example response in a comment

--- frontend/plotpoints/src/app/components/ReviewList.js
+++ frontend/plotpoints/src/app/components/ReviewList.js
@@ -9,6 +9,18 @@ export default function Reviews({ reviewData = [] }) {
     { id: 3, username: "Happy Viewer", text: "10/10 would recommend!" },
   ];*/
 
+  //Example response from backend:
+  /*
+  {
+      "review_id": 2,
+      "user_id": 5,
+      "media_id": 81,
+      "media_type": "book",
+      "rating": 3,
+      "review_text": "It truly is one of the islands of all time"
+    }
+  */
+
   if(reviewData.length === 0) {
     return <p>No reviews yet. Be the first to review!</p>;
   }
