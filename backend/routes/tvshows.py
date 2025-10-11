9ae0ee94ca8c258114014a5a7d6f885b893fa752
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-11 16:42:54 -0400

    remove to string function in avgrating call

--- backend/routes/tvshows.py
+++ backend/routes/tvshows.py
@@ -255,7 +255,7 @@ async def get_tvshow_streaming_links(tv_id: int):
 
 @router.get("/{tv_id}/average_rating")
 async def get_tvshow_average_rating(tv_id: int):
-    avg_rating = get_avg_rating(str(tv_id))
+    avg_rating = get_avg_rating(tv_id)
     if avg_rating is None:
         return {"tv_id": tv_id, "average_rating": 0}
     return {"tv_id": tv_id, "average_rating": avg_rating}
