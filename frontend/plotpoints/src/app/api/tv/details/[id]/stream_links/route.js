a468b883dc19b1b48360901b7cd741b0545695ca
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-10 22:04:14 -0400

    Created route to get stream links

---
+++ frontend/plotpoints/src/app/api/tv/details/[id]/stream_links/route.js
@@ -0,0 +1,11 @@
+export async function GET(request, { params }) {
+    const { id } = await params;
+
+    const res = await fetch(`${process.env.API_URL}/tvshows/${id}/streaming_links`);
+    if (!res.ok) {
+        return new Response("Failed to fetch TV show streaming links", { status: 500 });
+    }
+    const data = await res.json();
+    return new Response(JSON.stringify(data), { status: 200 });
+
+}
