347b03497583746818332cbd125ae51e3519f38a
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-12 12:33:20 -0400

    Created search route

---
+++ frontend/plotpoints/src/app/api/search/[query]/route.js
@@ -0,0 +1,17 @@
+export async function GET(request, { params }) {
+    const { query } = params;
+    const { searchParams } = new URL(request.url);
+    const page = searchParams.get("page") || 1;
+
+    //const apiUrl = `${process.env.API_URL}/search/all?query=${encodeURIComponent(query)}&page=${page}`;
+    const apiUrl = `${process.env.API_URL}/search/all?query=${encodeURIComponent(query)}`;//no page at least for now, just simple call
+    
+    const res = await fetch(apiUrl);
+
+    if (!res.ok) {
+        return new Response(JSON.stringify({ error: "Failed to fetch results" }), { status: res.status });
+    }
+
+    const data = await res.json();
+    return new Response(JSON.stringify(data), { status: 200 });
+}
