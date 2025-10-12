486c2eecb5be2279e6dda6e2f840c1fff1071641
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-11 21:38:14 -0400

    Created fetchStreamLinks util function

---
+++ frontend/plotpoints/src/utils/fetchStreamLinks.js
@@ -0,0 +1,13 @@
+export async function fetchStreamLinks(mediaType, id) {
+    try {
+        const res = await fetch(`${process.env.API_URL}/${mediaType}/${id}/streaming_links`);
+        if (!res.ok) {
+            throw new Error(`Failed to fetch streaming links for ${id}`);
+        }
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
