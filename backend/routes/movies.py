1c927a0b4b301af41e68791c112b318985eae201
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-11 22:03:09 -0400

    Added mgm+ amazon channel to streaming links

--- backend/routes/movies.py
+++ backend/routes/movies.py
@@ -34,6 +34,7 @@ STREAMING_LINKS = {
             "Tubi TV": "https://tubitv.com/",
             "Peacock": "https://www.peacocktv.com/",
             "fuboTV": "https://www.fubo.tv/",
+            "MGM+ Amazon Channel": "https://www.amazon.com/gp/video/channel/1cd832b6-c298-4106-b4ff-dc9ce63304ea"
         }
 
 
