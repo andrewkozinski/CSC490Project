fe8bbcc94f2a449ef8bbaac03dd185d0318112f5
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-11 21:50:55 -0400

    Updated fetch stream links just to contain flatrate (streaming platforms)

--- frontend/plotpoints/src/utils/fetchStreamLinks.js
+++ frontend/plotpoints/src/utils/fetchStreamLinks.js
@@ -1,10 +1,14 @@
-const fetchStreamLinks = async (mediaType, id) => {
+const fetchStreamLinks = async (mediaType, id, setStreamLinks) => {
     try {
-        const res = await fetch(`${process.env.API_URL}/${mediaType}/${id}/streaming_links`);
+        const res = await fetch(`/api/streamlinks/${mediaType}/${id}`);
         if (!res.ok) {
             throw new Error(`Failed to fetch streaming links for ${id}`);
         }
         const data = await res.json();
+
+        // Only keep flatrate, if no flatrate then set to just empty array
+        const flatrateLinks = Array.isArray(data.flatrate) ? data.flatrate : [];
+        setStreamLinks(flatrateLinks);
         return data;
     } catch (error) {
         console.error(error);
