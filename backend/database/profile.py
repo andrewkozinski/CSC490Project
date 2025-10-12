73f634c76ea626d23e9ba78db66d5a845eafd832
jsieb <jsiebor724@gmail.com>
2025-10-12 19:04:26 -0400

    Update profile pic

--- backend/database/profile.py
+++ backend/database/profile.py
@@ -34,3 +34,36 @@ def update_bio(user_id, new_bio):
 
     finally:
         connect.stop_connection(connection, cursor)
+
+def update_profile_pic(user_id, new_profile_pic):
+    connection, cursor = connect.start_connection()
+    if not connection or not cursor:
+        print("Failed to connect to database.")
+        return
+
+    try:
+        cursor.execute(
+            """
+            UPDATE PROFILE
+            SET PROFILE_PIC = :1
+            WHERE USER_ID = :2
+            """,
+            (new_profile_pic, user_id)
+        )
+
+        if cursor.rowcount == 0:  # no rows updated
+            print(f"Error: USER_ID {user_id} does not exist.")
+        else:
+            connection.commit()
+            print(f"Profile Pic for USER_ID {user_id} updated successfully to '{new_profile_pic}'.")
+
+    except oracledb.IntegrityError as e:
+        error_obj, = e.args
+        print("Integrity error:", error_obj.message)
+
+    except oracledb.Error as e:
+        error_obj, = e.args
+        print("Database error updating Profile Pic:", error_obj.message)
+
+    finally:
+        connect.stop_connection(connection, cursor)
