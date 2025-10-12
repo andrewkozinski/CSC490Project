531f20873c2ccbdcde651f8e10069986fc056acb
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-11 21:02:36 -0400

    Route updated to correctly create a comment

--- frontend/plotpoints/src/app/api/comments/under_review/[id]/post_comment/route.js
+++ frontend/plotpoints/src/app/api/comments/under_review/[id]/post_comment/route.js
@@ -1,18 +1,28 @@
+import { NextResponse } from "next/server";
+
 export async function POST(request, context) {
     const body = await request.json();
+    const { review_id, user_id, comment_text, jwt_token } = body;
 
     const res = await fetch(`${process.env.API_URL}/comments/create`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
+            "Authorization": `Bearer ${jwt_token}`,
         },
-        body: JSON.stringify(body),
+        body: JSON.stringify({
+            review_id,
+            user_id,
+            comment_text
+        }),
     });
 
     if (!res.ok) {
-        return new Response("Failed to create comment", { status: 500 });
+      const errorData = await res.json()
+      return NextResponse.json({ error: errorData.detail || 'Failed to create comment' }, { status: res.status })
     }
 
     const data = await res.json();
-    return new Response(JSON.stringify(data), { status: 200 });
+    return NextResponse.json(data);
+
 }
