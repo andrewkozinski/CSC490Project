21aefbde639c15bd23d46d23e4c2001cd600b60f
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-13 20:01:25 -0400

    Added votes to get_reviews_by_media_type

--- backend/routes/reviews.py
+++ backend/routes/reviews.py
@@ -82,6 +82,7 @@ async def get_reviews_by_media_type(media_type: str):
         user = await get_username_by_id(review["user_id"])
         review["username"] = user if user else "Unknown User"
         votes = vote.get_vote_by_review_id(review["review_id"])
+        review["votes"] = votes if votes else {"upvotes": 0, "downvotes": 0}
     return {"reviews": reviews_by_type}
 
 #Get all reviews by a media type and id
