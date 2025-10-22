851457ab7fac6aaac5a9deb993357415a6bbd245
[serrvl@farmingdale.edu] <serrvl@farmingdale.edu>
2025-10-22 14:28:12 -0400

    added column for profiles

--- frontend/plotpoints/src/app/myprofile/following/page.js
+++ frontend/plotpoints/src/app/myprofile/following/page.js
@@ -1,12 +1,14 @@
 import Footer from "@/app/components/Footer";
 import Header from "../../components/Header";
 
-export default function followers() {
-    return(
-        <div>
-        <Header></Header>
-        <div></div>
-        <Footer></Footer>
-        </div>
-    )
+export default function following() {
+  return (
+    <div>
+      <Header></Header>
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-3/4 h-full bottom-0 text-center shadow-lg mb-3 outline-transparent"></div>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 }
