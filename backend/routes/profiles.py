e9b76635880160ff8858ac96b3a8ac3b1dca1f0a
andrewkozinski <andrew.kozinski@optimum.com>
2025-10-23 14:21:24 -0400

    Added get user information api route

--- backend/routes/profiles.py
+++ backend/routes/profiles.py
@@ -10,3 +10,12 @@ async def get_username_by_id(user_id: int):
         return user["USERNAME"]
     else:
         raise HTTPException(status_code=404, detail="User not found")
+    
+
+@router.get("/get/user_information")
+async def get_user_info_by_id(user_id: int):
+    user = get_by_id(user_id)
+    if user:
+        return user
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
