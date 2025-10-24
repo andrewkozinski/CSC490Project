eb3f933b0ecabc85c8d5a1681aca0c2a3efb5e47
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-24 18:37:05 -0400

    Updated next.config

--- frontend/plotpoints/next.config.mjs
+++ frontend/plotpoints/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["books.google.com", "placehold.co", "covers.openlibrary.org", "image.tmdb.org"],
+        domains: ["books.google.com", "placehold.co", "covers.openlibrary.org", "image.tmdb.org", "idmldn7fblfn.objectstorage.us-ashburn-1.oraclecloud.com", "objectstorage.us-ashburn-1.oraclecloud.com"],
         dangerouslyAllowSVG: true
     },
 };
