8153b20b402787661a6119e799c42edececfdbd3
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-25 12:12:21 -0400

    Created follow route

---
+++ frontend/plotpoints/src/app/api/following/follow/[id]/route.js
@@ -0,0 +1,13 @@
+export async function PUT(request, context) {
+    const body = await request.json();
+    const { follow_id, jwt_token } = body;
+
+    const res = await fetch(`${process.env.API_URL}/follow/follow/${follow_id}?jwt_token=${encodeURIComponent(jwt_token)}`, {
+        method: 'PUT',
+    });
+    if (!res.ok) {
+        return new Response("Failed to update bio womp womp", { status: 500 });
+    }
+    const data = await res.json();
+    return new Response(JSON.stringify(data), { status: 200 });
+}
