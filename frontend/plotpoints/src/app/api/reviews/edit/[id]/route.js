68c1360c144e941ae00f5ced9d65ce236bb4d9d1
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-24 21:23:46 -0400

    Edit route now fully functioning

--- frontend/plotpoints/src/app/api/reviews/edit/[id]/route.js
+++ frontend/plotpoints/src/app/api/reviews/edit/[id]/route.js
@@ -1,7 +1,8 @@
 import { NextResponse } from 'next/server';
 
 export async function PUT(request, { params }) {
-  const review_id = params.id;
+  const params_content = await params;
+  const review_id = params_content.id;
   const { review_text, jwt_token } = await request.json();
 
   const response = await fetch(`${process.env.API_URL}/reviews/edit/${review_id}`, {
@@ -10,12 +11,12 @@ export async function PUT(request, { params }) {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${jwt_token}`,
     },
-    body: JSON.stringify({ review_text, jwt_token }),
+    body: JSON.stringify({ review_id, review_text, jwt_token }),
   });
 
   if (!response.ok) {
     const error = await response.json();
-    return NextResponse.json({ error: error.detail || 'An error occurred, review not found.' }, { status: 404 });
+    return NextResponse.json({ error: error.detail || 'An error occurred, review not found.' }, { status: response.status });
   }
 
   const data = await response.json();
