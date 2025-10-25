b3bccde52f24c7bd861b2dde45492e488b59c71c
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-25 13:59:07 -0400

    Logic now exists to fetch and displayt followers / following information

--- frontend/plotpoints/src/app/profile/[id]/followers/page.js
+++ frontend/plotpoints/src/app/profile/[id]/followers/page.js
@@ -1,12 +1,43 @@
+"use client";
 import Footer from "@/app/components/Footer";
-import Header from "../../components/Header";
+import Header from "@/app/components/Header";
+import { useEffect, useState } from "react";
+import React from "react";
+import { getFollowers } from "@/lib/following";
+import FollowProfile from "@/app/components/FollowProfile";
 
-export default function followers() {
+export default function followers({ params }) {
+
+  //Grab the ID from the URL
+  const unwrappedParams = React.use(params);
+  const id = unwrappedParams.id; //user id from the url
+
+  //Fetch followers data
+  const [followersData, setFollowersData] = useState([]);
+
+  useEffect(() => {
+    async function fetchFollowers() {
+      try {
+        const data = await getFollowers(id);
+        setFollowersData(data.followers);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching followers data:", error);
+      }
+    }
+    fetchFollowers();
+  }, []);
+
   return (
     <div>
       <Header></Header>
       <div className="flex items-center justify-center h-screen">
         <div className="w-3/4 h-full bottom-0 text-center shadow-lg mb-3 outline-transparent">
+
+          {followersData.map((user, index) => (
+            <FollowProfile key={index} name={user.username} desc={user.bio} user_id={user.id} pfp_url={user.profile_pic_url}></FollowProfile>
+          ))}
+          
         </div>
       </div>
       <Footer></Footer>
