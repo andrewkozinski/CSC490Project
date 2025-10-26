223389e3cbc95a4d0f099b148f9a99285bb02b5d
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-10-26 12:55:42 -0400

    Added new tenna gifs and a function to get a random tenna loading gif

---
+++ frontend/plotpoints/src/lib/random_tenna_loading.js
@@ -0,0 +1,12 @@
+/**
+ * Returns a random Tenna image for loading states because tenna smiling
+ */
+export const randomTennaLoading = async () => {
+    const tennaImages = [
+        "/images/spr_tenna_t_pose_big.gif",
+        "/images/spr_tenna_dance_cabbage_big.gif",
+        "/images/spr_tenna_dance_cabbage_big_fast.gif",
+        "/images/tennabow.gif",];
+    const randomIndex = Math.floor(Math.random() * tennaImages.length);
+    return tennaImages[randomIndex];  
+}
