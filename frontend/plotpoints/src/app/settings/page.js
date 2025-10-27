e7681a222ca3a93842d0b998674e028a62c51ebf
[serrvl@farmingdale.edu] <serrvl@farmingdale.edu>
2025-10-27 16:57:09 -0400

    settings page change

--- frontend/plotpoints/src/app/settings/page.js
+++ frontend/plotpoints/src/app/settings/page.js
@@ -6,15 +6,18 @@ import Footer from "../components/Footer";
 import GenreContainer from "../components/GenreContainer";
 import Image from "next/image";
 import Review from "../components/HomepageReview";
-import '../components/Homepage.css';
-
+import "../components/Homepage.css";
 
 export default function Home() {
-return(
+  return (
     <div>
-    <Header></Header>
-    <p>Settings!</p>
-    <Footer></Footer>
+      <Header></Header>
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-3/4 h-full bottom-0 text-center shadow-lg mb-3 outline-transparent">
+          <h1 className="text-3xl font-bold text-center mt-10">Settings</h1>
+        </div>
+      </div>
+      <Footer></Footer>
     </div>
-);
+  );
 }
