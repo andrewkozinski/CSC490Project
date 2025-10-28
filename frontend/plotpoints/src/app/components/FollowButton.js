46d4bb634bb479d0cbda99d5f899188b518ac0ff
[serrvl@farmingdale.edu] <serrvl@farmingdale.edu>
2025-10-28 04:58:09 -0400

    unfollow and follow work for followbutton

--- frontend/plotpoints/src/app/components/FollowButton.js
+++ frontend/plotpoints/src/app/components/FollowButton.js
@@ -1,48 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { isFollowing } from "@/lib/following";
-
-const followSVG = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth="1.5"
-    stroke="currentColor"
-    className="w-6 h-6 mr-2"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 
-        3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 
-        19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 
-        12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
-    />
-  </svg>
-);
-
-const followingSVG = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth="1.5"
-    stroke="currentColor"
-    className="w-6 h-6 mr-2"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 
-        7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 
-        0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 
-        21a8.966 8.966 0 0 1-5.982-2.275M15 
-        9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-    />
-  </svg>
-);
+import { followUser, unfollowUser, isFollowing } from "@/lib/following";
 
 export default function FollowButton({ profileId, currentUserId, jwtToken }) {
   if (profileId === currentUserId) return;
@@ -52,12 +11,11 @@ export default function FollowButton({ profileId, currentUserId, jwtToken }) {
     if (profileId === currentUserId) return; // Your profile
     if (!jwtToken) return; // Not logged in
     const checkFollowingStatus = async () => {
-      console.log("Checking if user follows this profile...");
       console.log(profileId, "and", currentUserId);
       try {
         const data = await isFollowing(profileId, jwtToken);
         console.log("Follow check response:", data);
-        setIsUserFollowing(data.isFollowing);
+        setIsUserFollowing(data.is_following);
       } catch (err) {
         console.error("Error checking follow status:", err);
       }
@@ -73,7 +31,7 @@ export default function FollowButton({ profileId, currentUserId, jwtToken }) {
   if (!isUserFollowing && profileId != currentUserId) {
     return (
       <button
-        onClick={() => console.log("Follow button clicked for", profileId)}
+        onClick={async () => console.log(await followUser(profileId, jwtToken))}
         className="px-4 py-2 rounded text-white flex flex-row justify-center transition cursor-pointer bg-[var(--color-brown)]"
       >
         <svg
@@ -91,6 +49,32 @@ export default function FollowButton({ profileId, currentUserId, jwtToken }) {
           />
         </svg>
         <p>Follow</p>
+      </button>
+    );
+  }
+  if (isUserFollowing && profileId != currentUserId) {
+    return (
+      <button
+        onClick={async () =>
+          console.log(await unfollowUser(profileId, jwtToken))
+        }
+        className="px-4 py-2 rounded text-white flex flex-row justify-center transition cursor-pointer bg-[var(--color-brown)]"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-6 mr-1"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM4 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 10.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
+          />
+        </svg>
+        <p>Unfollow</p>
       </button>
     );
   }
