9fd5f908833fcf86a3be9e4a51acd4b00fb0f4aa
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-06 20:14:10 -0500

    Created routes file for notifications

---
+++ backend/routes/notifications.py
@@ -0,0 +1,34 @@
+from fastapi import Depends, FastAPI, HTTPException
+from routes.auth import verify_jwt_token, get_user_id_from_token
+from database import notifications
+router = FastAPI()
+
+
+@router.get("/notifications/user_id/{user_id}")
+async def get_notifications_by_user_id(jwt_token: str):
+    # Verify JWT token
+    #verify_jwt_token(jwt_token)
+    user_id = get_user_id_from_token(jwt_token)
+    notifs = notifications.get_notifications_by_user_id(user_id)
+    if notifs is not None:
+        return {"notifications": notifs}
+    raise HTTPException(status_code=500, detail="Error fetching notifications")
+
+@router.get("/notifications/count")
+async def get_notification_count(jwt_token: str):
+    # Verify JWT token
+    #verify_jwt_token(jwt_token)
+    user_id = get_user_id_from_token(jwt_token)
+    count = notifications.get_notification_count_by_user_id(user_id)
+    if count is not None:
+        return {"unread_count": count}
+    raise HTTPException(status_code=500, detail="Error fetching notification count")
+
+@router.put("/notifications/read/{noti_id}")
+async def read_notification(noti_id: int, jwt_token: str):
+    # Verify JWT token
+    verify_jwt_token(jwt_token)
+    result = notifications.read_notification(noti_id)
+    if result is not None:
+        return {"message": "Notification marked as read"}
+    raise HTTPException(status_code=500, detail="Error marking notification as read")
