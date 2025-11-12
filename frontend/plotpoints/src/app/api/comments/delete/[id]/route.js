7a6bff8ac971691002f0170a546a091c03cbffe8
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-11 22:43:50 -0500

    Added in the API routes for editing and deleting comments

---
+++ frontend/plotpoints/src/app/api/comments/delete/[id]/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+
+export async function DELETE(request, context) {
+    const params = await context.params;
+    const review_id = params.id;
+    const { jwt_token } = await request.json();
+
+    const response = await fetch(`${process.env.API_URL}/comments/delete/${review_id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${jwt_token}`,
+        },
+        body: JSON.stringify({
+            review_id,
+            jwt_token 
+        }),
+    });
+
+    if (!response.ok) {
+        const error = await response.json();
+        return NextResponse.json({ error: error.detail || 'Review not found' }, { status: 404 });
+    }
+
+    const data = await response.json();
+    return NextResponse.json(data);
+}
