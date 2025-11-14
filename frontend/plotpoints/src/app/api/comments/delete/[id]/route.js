fadab5a97087f167b6ae68d7f67219ccc22f287d
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-13 20:24:30 -0500

    Fixed deletion route

--- frontend/plotpoints/src/app/api/comments/delete/[id]/route.js
+++ frontend/plotpoints/src/app/api/comments/delete/[id]/route.js
@@ -2,17 +2,17 @@ import { NextResponse } from 'next/server';
 
 export async function DELETE(request, context) {
     const params = await context.params;
-    const review_id = params.id;
+    const comment_id = params.id;
     const { jwt_token } = await request.json();
 
-    const response = await fetch(`${process.env.API_URL}/comments/delete/${review_id}`, {
+    const response = await fetch(`${process.env.API_URL}/comments/delete/`, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${jwt_token}`,
         },
         body: JSON.stringify({
-            review_id,
+            comment_id,
             jwt_token 
         }),
     });
