df5ae91bc47d14d5c2d8c21aed44fbad2e08134b
Kathleen Tan <tank@farmingdale.edu>
2025-11-15 18:37:10 -0500

    Review text toggle ui done, not functional

--- frontend/plotpoints/src/app/settings/page.js
+++ frontend/plotpoints/src/app/settings/page.js
@@ -3,15 +3,23 @@
 import { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Switch from "../components/Switch";
 import "../components/Homepage.css";
 
 export default function Settings() {
   return (
     <div>
       <Header></Header>
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-center max-h-screen">
         <div className="w-3/4 h-full bottom-0 text-center mb-3 outline-transparent">
           <h1 className="text-3xl font-bold text-center mt-10">Settings</h1>
+          <div className="flex flex-row w-full items-center justify-center gap-25 p-20">
+            <h2 className="font-bold text-start">Review Text
+              <p className="text-xs font-thin">Toggle off to display only rating values for media.</p>
+            </h2>
+            <Switch/>
+          </div>
+          
         </div>
       </div>
       <Footer></Footer>
