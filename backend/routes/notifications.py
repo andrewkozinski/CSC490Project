064172800c0c27c630f8c9a25e1117caacbdfa1b
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-14 21:52:42 -0500

    Changed notifications to just take in a user id get notification count and get notifications by user

--- backend/routes/notifications.py
+++ backend/routes/notifications.py
@@ -1,24 +1,37 @@
 from fastapi import HTTPException, APIRouter
 from routes.auth import verify_jwt_token, get_user_id_from_token
+from database.reviews import get_review_by_review_id
+from database.comments import get_comment_by_comm_id
 from database import notifications
 
 router = APIRouter()
 
 @router.get("/notifications/user_id/{user_id}")
-async def get_notifications_by_user_id(jwt_token: str):
+async def get_notifications_by_user_id(user_id: str):
     # Verify JWT token
     #verify_jwt_token(jwt_token)
-    user_id = get_user_id_from_token(jwt_token)
+    #user_id = get_user_id_from_token(jwt_token)
     notifs = notifications.get_notifications_by_user_id(user_id)
     if notifs is not None:
-        return {"notifications": notifs}
+        #return {"notifications": notifs}
+        for notif in notifs:
+            # Get the review associated with a notification
+            if notif["review_id"]:
+                review = get_review_by_review_id(notif["review_id"])
+                if review:
+                    notif["review_content"] = review
+            if notif["comment_id"]:
+                comment = get_comment_by_comm_id(notif["comment_id"])
+                if comment:
+                    notif["comment_content"] = comment
+        return notifs
     raise HTTPException(status_code=500, detail="Error fetching notifications")
 
 @router.get("/notifications/count")
-async def get_notification_count(jwt_token: str):
+async def get_notification_count(user_id: str):
     # Verify JWT token
     #verify_jwt_token(jwt_token)
-    user_id = get_user_id_from_token(jwt_token)
+    #user_id = get_user_id_from_token(jwt_token)
     count = notifications.get_notification_count_by_user_id(user_id)
     if count is not None:
         return {"unread_count": count}
