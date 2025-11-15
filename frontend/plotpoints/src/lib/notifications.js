32ba53595f15cd94c6701102d0abccb3414ac649
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-14 22:09:27 -0500

    Created route and helper function to get notifications by a user's id

---
+++ frontend/plotpoints/src/lib/notifications.js
@@ -0,0 +1,16 @@
+
+export const getNotifications = async (userId) => {
+    const res = await fetch(`/api/notifications/get/${userId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch notifications');
+    }
+
+    const data = await res.json();
+    return data;
+};
