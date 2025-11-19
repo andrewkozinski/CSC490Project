0d6f1c5891431a2d06e89c8377a61a75a3459e14
[serrvl@farmingdale.edu] <serrvl@farmingdale.edu>
2025-11-19 03:56:53 -0500

    added useID() for Switch

--- frontend/plotpoints/src/app/components/Switch.js
+++ frontend/plotpoints/src/app/components/Switch.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import React, { useId } from 'react';
 import './Switch.css';
 
 const Switch = ({ isOn, handleToggle }) => {
+  const id = useId(); // generates a unique ID
   return (
     <>
       <input
         checked={isOn}
         onChange={handleToggle}
         className="react-switch-checkbox"
-        id={`react-switch-new`}
+        id={id}
         type="checkbox"
       />
       <label
         style={{ background: isOn && '#99dfe8ff' }}
         className="react-switch-label"
-        htmlFor={`react-switch-new`}
+        htmlFor={id}
       >
-        <span className={`react-switch-button`} />
+        <span className="react-switch-button" />
       </label>
     </>
   );
