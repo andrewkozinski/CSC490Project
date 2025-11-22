ab6c8c53963e54562b327c7fa7568a13c4adf0a9
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-22 14:57:46 -0500

    Added recommended movies carousel

--- frontend/plotpoints/src/app/movies/page.js
+++ frontend/plotpoints/src/app/movies/page.js
@@ -5,14 +5,21 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Carousel from "../components/CategoryCarousel";
 import "../components/Homepage.css";
+import { getRecommendedMovies } from "@/lib/recommendations";
+import { useSession } from "next-auth/react";
+import SkeletonImage from "../components/SkeletonImage";
+import { Skeleton } from "@mui/material";
 
 export default function Movies() {
+
+  const { data: session } = useSession();
 
   const [horrorMovies, setHorrorMovies] = useState([]);
   const [historyMovies, setHistoryMovies] = useState([]);
   const [comedyMovies, setComedyMovies] = useState([]);
   const [sciFiMovies, setSciFiMovies] = useState([]);
   const [upcomingMovies, setUpcomingMovies] = useState([]);
+  const [recommendedMovies, setRecommendedMovies] = useState([]);
 
   //Handles fetching movies from the backend
   useEffect(() => {
@@ -48,9 +55,25 @@ export default function Movies() {
     fetchMovies("comedy", setComedyMovies);
     fetchMovies("science fiction", setSciFiMovies);
     fetchUpcomingMovies();
-
-    
   }, []);
+
+  // Fetch recommended movies based on user ID
+  useEffect(() => {
+    const fetchRecommendedMovies = async () => {
+      console.log("Fetching recommended movies for user:", session?.user?.id);
+      if (session?.user?.id) {
+        try {
+          const data = await getRecommendedMovies(session.user.id);
+          setRecommendedMovies(data || []);
+          console.log("RECOMMENDED MOVIES:");
+          console.log(data);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
+    fetchRecommendedMovies();
+  }, [session?.user]);
 
   return (
     <div>
@@ -128,6 +151,29 @@ export default function Movies() {
             />
           ))}
         </Carousel>
+
+        {/* Check if user is logged in, if so show recommendations */}
+        {session?.user && (
+          <Carousel label="Recommended Movies">
+          {/* If 0 show skeleton cards */}
+          {recommendedMovies.length === 0 ? (
+            Array.from({ length: 20 }).map((_, index) => (
+              <SkeletonImage key={index} />
+            ))
+          ) : (
+            recommendedMovies.map((movie) => (  
+              <img
+                key={movie.id}
+                src={movie.img}
+                title={movie.title}
+                className="image"
+                onClick={() => window.location.href = `/movies/review/${movie.id}`}
+                style={{ cursor: 'pointer' }}
+              />
+            ))
+          )}
+          </Carousel>
+        )}
         
       </main>
       <Footer />
