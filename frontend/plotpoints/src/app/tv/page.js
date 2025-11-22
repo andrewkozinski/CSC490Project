79bd5ce9c379d245f46574349e92371620c13862
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-22 15:02:06 -0500

    Added recommended shows carousel

--- frontend/plotpoints/src/app/tv/page.js
+++ frontend/plotpoints/src/app/tv/page.js
@@ -1,18 +1,23 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Carousel from "../components/CategoryCarousel";
 import "../components/Homepage.css";
+import SkeletonImage from "../components/SkeletonImage";
 
 export default function TV() {
+
+  const { data: session } = useSession();
 
   const [kidsShows, setKidsShows] = useState([]);
   const [dramaShows, setDramaShows] = useState([]);
   const [comedyShows, setComedyShows] = useState([]);
   const [crimeShows, setCrimeShows] = useState([]);
   const [airingTodayShows, setAiringTodayShows] = useState([]);
+  const [recommendedShows, setRecommendedShows] = useState([]);
 
   //Handles fetching tv shows from the backend
   useEffect(() => {
@@ -49,6 +54,25 @@ export default function TV() {
     fetchShows("crime", setCrimeShows);    
     fetchAiringTodayShows();
   }, []);
+
+  // Fetch recommended TV shows based on user ID
+  useEffect(() => {
+    const fetchRecommendedShows = async () => {
+      if (session?.user?.id) {
+        try {
+          const res = await fetch(`/api/recommendations/tvshows?userId=${encodeURIComponent(session.user.id)}`);
+          if (!res.ok) throw new Error("Failed to fetch recommended TV shows");
+          const data = await res.json();
+          setRecommendedShows(data || []);
+          console.log("RECOMMENDED TV SHOWS:");
+          console.log(data);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
+    fetchRecommendedShows();
+  }, [session?.user]);
 
   return (
     <div>
@@ -115,6 +139,29 @@ export default function TV() {
             />
           ))}
         </Carousel>
+
+        {/* Check if user is logged in, if so show recommendations */}
+        {session?.user && (
+          <Carousel label="Recommended Shows">
+          {/* If 0 show skeleton cards */}
+          {recommendedShows.length === 0 ? (
+            Array.from({ length: 20 }).map((_, index) => (
+              <SkeletonImage key={index} />
+            ))
+          ) : (
+            recommendedShows.map((show) => (  
+              <img
+                key={show.id}
+                src={show.img}
+                title={show.name}
+                className="image"
+                onClick={() => window.location.href = `/tv/review/${show.id}`}
+                style={{ cursor: 'pointer' }}
+              />
+            ))
+          )}
+          </Carousel>
+        )}
         
       </main>
       <Footer />
