8263ca58af26bbbf576a693872765cbf305b3473
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-23 19:23:35 -0500

    Changed settings function call to be get settings call and accounted for dark mode enabled setting

--- backend/routes/settings.py
+++ backend/routes/settings.py
@@ -13,14 +13,18 @@ async def get_review_text_setting(jwt_token: str):
     user_id = get_user_id_from_token(jwt_token)
 
     # Fetch the setting from the database
-    setting = settings_text.get_settings_text_by_user_id(user_id)
+    setting = settings_text.get_settings_by_user_id(user_id)
 
     if setting is None:
-        return {"review_text_enabled": False}
+        return {
+            "review_text_enabled": True,
+            "dark_mode_enabled": False
+                }
 
     return {
         "setting_id": setting["setting_id"],
-        "review_text_enabled": bool(setting["review_text_enabled"])
+        "review_text_enabled": bool(setting["review_text_enabled"]),
+        "dark_mode_enabled": bool(setting["dark_mode_enabled"])
     }
 
 @router.put("/review_text")
