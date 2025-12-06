f0a5400ac1707ef2c33562d7b6d10630852eb0f6
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-12-06 18:27:02 -0500

    Hopefully not broken

--- frontend/plotpoints/src/app/profile/[id]/page.js
+++ frontend/plotpoints/src/app/profile/[id]/page.js
@@ -197,7 +197,7 @@ export default function ProfilePage( {params} ){
         <div>   
             <Header/>
             <div className="flex flex-row gap-5 min-h-screen">
-                <div className="flex flex-cols mt-10 ml-10 max-w-1/5 max-h-fit border place-items-start">
+                <div className="flex flex-col items-center gap-6 mt-10 ml-10 w-1/5">
                     <Image 
                     className={`flex aspect-square rounded-full mb-2 border-2 justify-center
                         ${darkOn ? "border-[#F3E9DC]" : "border-black"}`} 
@@ -205,127 +205,128 @@ export default function ProfilePage( {params} ){
                     alt="User Image"
                     width="230"
                     height="230"/>
-                    
-            
 
-                    {/* Get username */}
-                    <div className="grid grid-rows-5 gap-2">
-                        <div className="flex flex-row justify-center items-center w-1/5">
-                            <h1 className="text-3xl text-center inria-serif-regular pr-3">{profileDetails?.username || "Error: Username not found"}</h1>
-                            
-                            {/*Verify current page is the current users profile*/}
-                            {profileDetails?.user_id === session?.user?.id && (
-                            <svg 
-                                xmlns="http://www.w3.org/2000/svg" 
-                                fill="none" 
-                                viewBox="0 0 24 24" 
-                                strokeWidth="1" 
-                                stroke="currentColor" 
-                                className="size-9 transition-transform ease-in-out place-self-center will-change-transform hover:scale-115 hover:cursor-pointer origin-center"
-                                onClick={() => setShowModal(true)}>
-                                <path 
-                                strokeLinecap="round" 
-                                strokeLinejoin="round" 
-                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" 
+                    {/*Username details & edit modal stuff*/}
+                    <div className="flex items-center gap-2">
+                        <h1 className="text-3xl text-center inria-serif-regular">{profileDetails?.username || "Error: Username not found"}</h1>
+
+                        {/*Verify current page is the current users profile*/}
+                        {profileDetails?.user_id === session?.user?.id && (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1"
+                                stroke="currentColor"
+                                className="size-9 hover:scale-110 transition cursor-pointer"
+                                onClick={() => setShowModal(true)}
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0  0 1 5.25 6H10"
                                 />
-                            </svg> )}
+                            </svg>
+                        )}
+                    </div>
 
-                                {showModal &&
-                                <Modal onClose={() => setShowModal(false)}>
-                                    <h1 className="text-2xl text-center">Edit Profile</h1>
-                                    <div className="flex flex-col w-full">   
-                                        <div className="flex flex-row w-full justify-around items-center mt-5">
-                                            <Image 
-                                                className={`aspect-square rounded-full mb-5 border-2 
+                    {/*Edit modal*/}
+                    {showModal &&
+                        <Modal onClose={() => setShowModal(false)}>
+                            <h1 className="text-2xl text-center">Edit Profile</h1>
+                            <div className="flex flex-col w-full">
+                                <div className="flex flex-row w-full justify-around items-center mt-5">
+                                    <Image
+                                        className={`aspect-square rounded-full mb-5 border-2 
                                                      ${darkOn ? "border-[#F3E9DC]" : "border-black"}`}
-                                                src={imageFile ? URL.createObjectURL(imageFile) : profilePicture} /*If the user selects a file, use the selected file*/
-                                                alt="User Image"
-                                                width="170"
-                                                height="170">
-                                            </Image> 
+                                        src={imageFile ? URL.createObjectURL(imageFile) : profilePicture} /*If the user selects a file, use the selected file*/
+                                        alt="User Image"
+                                        width="170"
+                                        height="170">
+                                    </Image>
 
-                                            {/*File input for images, hidden*/}
-                                            <input type="file" id="profileImageUpload" name="profileImageUpload" accept="image/png, image/jpeg" className="hidden" onChange={handleImageChange}/>
-                                            <button className={`blue text-sm btn-shadow py-1 px-5 w-fit h-fit rounded-md transition hover:cursor-pointer hover:bg-[#B0E0E6]
-                                                ${ darkOn ? "text-black": ""}`}
-                                            onClick={async () => {
-                                                document.getElementById('profileImageUpload').click();
-                                            }}
-                                            >
-                                            Choose image...
-                                            </button>
-                                        </div>
-                                        <p className="text-sm font-bold ml-9">Bio</p>                               
-                                        <textarea
-                                        placeholder="Write a bio"
-                                        value={modalBio}
-                                        maxLength={144}
-                                        onChange={(e) => setModalBio(e.target.value)}
-                                        className={`w-6/7 text-sm bg-[#dfcdb59e] rounded-sm h-30 p-2 resize-none focus:outline-none place-self-center
-                                            ${ darkOn ? "bg-black" : "bg-[#dfcdb59e]"}`}
-                                        />
-                                        <button
-                                        className="blue text-sm text-black btn-shadow m-4 py-1 px-5 w-fit rounded-md place-self-center"
-                                        //onClick to save image and bio
+                                    {/*File input for images, hidden*/}
+                                    <input type="file" id="profileImageUpload" name="profileImageUpload" accept="image/png, image/jpeg" className="hidden" onChange={handleImageChange} />
+                                    <button className={`blue text-sm btn-shadow py-1 px-5 w-fit h-fit rounded-md transition hover:cursor-pointer hover:bg-[#B0E0E6]
+                                                ${darkOn ? "text-black" : ""}`}
                                         onClick={async () => {
-
-                                            //Profile picture update goes here
-                                            if(imageFile) {
-                                                
-                                                try {
-                                                    const result = await uploadProfilePicture(imageFile, session?.accessToken);
-                                                    console.log("Profile picture uploaded successfully:", result);
-                                                    setProfilePicture(result.pfp_url);
-                                                } catch (error) {
-                                                    console.error("Error uploading profile picture:", error);
-                                                }
-
-                                            }
-
-                                            //Check if bio has changed
-                                            if (modalBio !== profileDetails?.bio) {
-                                                //API call to save bio
-                                                const response = await fetch('/api/profiles/update/bio', {
-                                                    method: 'PUT',
-                                                    headers: { 'Content-Type': 'application/json' },
-                                                    body: JSON.stringify({ new_bio: modalBio, jwt_token: session?.accessToken })
-                                                });
-
-                                                if (!response.ok) {
-                                                    console.error("Failed to update bio");
-                                                    return;
-                                                }
-
-                                                //Now update local profile details state
-                                                setProfileDetails((prevDetails) => ({
-                                                    ...prevDetails,
-                                                    bio: modalBio
-                                                }));
-
-                                            }
-
-                                            //close modal
-                                            setShowModal(false);
+                                            document.getElementById('profileImageUpload').click();
                                         }}
-                                        > 
-                                        Save </button>
-                                    </div>
-                                </Modal>
-                                }
-                        </div>
-                        <div className="overflow-y-auto max-w-xs">
-                            <p className="text-center border-y-1 self-center break-words">{profileDetails?.bio || "No description."}</p>
-                        </div>
-                        <div className="grid grid-cols-2">
-                            <Link className="text-center m-1 hover:text-[#ffa2e9]" href={`/profile/${id}/followers`}>{followers.length}<br></br>Followers</Link>
-                            <Link className="text-center m-1 hover:text-[#ffa2e9]" href={`/profile/${id}/following`}>{following.length}<br></br>Following</Link>
-                        </div>
-                        <FollowButton profileId={id} currentUserId={session?.user?.id} jwtToken={session?.accessToken}></FollowButton>
-                        <BlockButton profileId={id} currentUserId={session?.user?.id} jwtToken={session?.accessToken}></BlockButton>
- 
+                                    >
+                                        Choose image...
+                                    </button>
+                                </div>
+                                <p className="text-sm font-bold ml-9">Bio</p>
+                                <textarea
+                                    placeholder="Write a bio"
+                                    value={modalBio}
+                                    maxLength={144}
+                                    onChange={(e) => setModalBio(e.target.value)}
+                                    className={`w-6/7 text-sm bg-[#dfcdb59e] rounded-sm h-30 p-2 resize-none focus:outline-none place-self-center
+                                            ${darkOn ? "bg-black" : "bg-[#dfcdb59e]"}`}
+                                />
+                                <button
+                                    className="blue text-sm text-black btn-shadow m-4 py-1 px-5 w-fit rounded-md place-self-center"
+                                    //onClick to save image and bio
+                                    onClick={async () => {
+
+                                        //Profile picture update goes here
+                                        if (imageFile) {
+
+                                            try {
+                                                const result = await uploadProfilePicture(imageFile, session?.accessToken);
+                                                console.log("Profile picture uploaded successfully:", result);
+                                                setProfilePicture(result.pfp_url);
+                                            } catch (error) {
+                                                console.error("Error uploading profile picture:", error);
+                                            }
+
+                                        }
+
+                                        //Check if bio has changed
+                                        if (modalBio !== profileDetails?.bio) {
+                                            //API call to save bio
+                                            const response = await fetch('/api/profiles/update/bio', {
+                                                method: 'PUT',
+                                                headers: { 'Content-Type': 'application/json' },
+                                                body: JSON.stringify({ new_bio: modalBio, jwt_token: session?.accessToken })
+                                            });
+
+                                            if (!response.ok) {
+                                                console.error("Failed to update bio");
+                                                return;
+                                            }
+
+                                            //Now update local profile details state
+                                            setProfileDetails((prevDetails) => ({
+                                                ...prevDetails,
+                                                bio: modalBio
+                                            }));
+
+                                        }
+
+                                        //close modal
+                                        setShowModal(false);
+                                    }}
+                                >
+                                    Save </button>
+                            </div>
+                        </Modal>
+                    } {/*End of edit modal*/}
+
+                    <div className="text-center px-2 break-words w-full border-y py-2">
+                        <p className="">{profileDetails?.bio || "No description."}</p>
+                    </div>
+
+                    <div className="grid grid-cols-2">
+                        <Link className="text-center m-1 hover:text-[#ffa2e9]" href={`/profile/${id}/followers`}>{followers.length}<br></br>Followers</Link>
+                        <Link className="text-center m-1 hover:text-[#ffa2e9]" href={`/profile/${id}/following`}>{following.length}<br></br>Following</Link>
                     </div>
                     
-                </div>
+                    <FollowButton profileId={id} currentUserId={session?.user?.id} jwtToken={session?.accessToken}></FollowButton>
+                    <BlockButton profileId={id} currentUserId={session?.user?.id} jwtToken={session?.accessToken}></BlockButton>
+                    
+                </div> {/*End of left profile column*/}
                 
                 <div className="grid w-150 m-15 h-fit">
                     <h1 className="text-md text-start whitespace-nowrap mb-5">Recent Reviews</h1>
