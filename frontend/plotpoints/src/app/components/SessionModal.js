24544a0796edecf229983ca56ffbc896aa4dacc2
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-12-08 20:50:32 -0500

    Updated SessionModal to take in setShowexpiredsessionmodal function

--- frontend/plotpoints/src/app/components/SessionModal.js
+++ frontend/plotpoints/src/app/components/SessionModal.js
@@ -2,7 +2,7 @@ import Modal from "@/app/components/Modal";
 import Link from "next/link";
 import "./Header.css";
 
-export default function SessionModal({ onClose }) {
+export default function SessionModal({ onClose, setShowExpiredSessionModal }) {
     return (
         <Modal onClose={onClose}>
             <h1 className="text-2xl text-center">Expired Session</h1>
@@ -11,8 +11,8 @@ export default function SessionModal({ onClose }) {
                 You must be signed in to continue.
                 </p>
                 <div className="flex flex-row justify-center items-center pb-5">
-                    <Link className="text fields blue btn-shadow mr-10 mt-1" href="/signup" onClick={() => onClose()}>Sign Up</Link>
-                    <Link className="text fields blue btn-shadow -ml-5" href="/signin" onClick={() => onClose()}>Sign In</Link>
+                    <Link className="text fields blue btn-shadow mr-10 mt-1" href="/signup" onClick={() => setShowExpiredSessionModal(false)}>Sign Up</Link>
+                    <Link className="text fields blue btn-shadow -ml-5" href="/signin" onClick={() => setShowExpiredSessionModal(false)}>Sign In</Link>
                 </div>
 
             </div>
