9b699ffad1cb02450c8bb9b4820e816bb2f0888c
Kathleen Tan <tank@farmingdale.edu>
2025-12-10 15:56:11 -0500

    Made reviews not viewable if blocked

--- frontend/plotpoints/src/app/components/ProfileReview.js
+++ frontend/plotpoints/src/app/components/ProfileReview.js
@@ -1,44 +1,62 @@
 import Star from "./Star";
 import ReviewText from "./ReviewText";
 import { useSettings } from "../context/SettingsProvider";
+import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
-import "./Profile.css";
 import {useRouter} from "next/navigation";
+import { isBlocked } from "@/lib/blocking";
+import "./Profile.css";
 
 export default function ProfileReview({ reviewData }) {
 
     const router = useRouter();
 
-    const {title, img, review_text, rating, media_type, media_id} = reviewData || {
+    const {
+        title,
+        img,
+        review_text,
+        username,
+        profile_pic_url,
+        rating,
+        media_type,
+        media_id,
+        user_id
+    } = reviewData || {
         title: "Superman",
         img: "https://image.tmdb.org/t/p/w500/wPLysNDLffQLOVebZQCbXJEv6E6.jpg",
         review_text: "An awesome take on a classic superhero story!",
+        username: "Username",
+        profile_pic_url: "https://objectstorage.us-ashburn-1.oraclecloud.com/n/idmldn7fblfn/b/plotpoint-profile-pic/o/def_profile/Default_pfp.jpg",
         rating: 5,
         media_type: "movie",
-        media_id: 1061474
+        media_id: 1061474,
+        user_id: 5
     };
     
     // const showReviewText = true;
     const { reviewText: showReviewText } = useSettings();
-
     const { darkMode: darkOn} = useSettings();
 
+    const { data: session } = useSession();
+    const currentUserId = session?.user?.id;
     const [isBlockedUser, setIsBlockedUser] = useState(false);
 
-    //   useEffect(() => {
-    //     if (!currentUserId || !jwtToken || userId === currentUserId) return;
-    
-    //     const fetchBlockedStatus = async () => {
-    //       try {
-    //         const data = await isBlocked(userId, currentUserId);
-    //         setIsBlockedUser(data.is_blocked);
-    //       } catch (err) {
-    //         console.error("Error checking block status:", err);
-    //       }
-    //     };
-    //     fetchBlockedStatus();
-    //   }, [userId, currentUserId, jwtToken]);
+      useEffect(() => {
+              if (!currentUserId || user_id === currentUserId) return;
+      
+              const fetchBlockedStatus = async () => {
+                  try {
+                      const data = await isBlocked(user_id, currentUserId);
+                      setIsBlockedUser(data.is_blocked);
+                  } catch (err) {
+                      console.error("Error checking block status:", err);
+                  }
+              };
+      
+              fetchBlockedStatus();
+          }, [user_id, currentUserId]);
 
+          const displayText = isBlockedUser ? "This review is from a blocked user." : review_text;
     return (
         <div className="flex flex-row rounded-[1px] max-w-full gap-4">
             <img
@@ -72,12 +90,9 @@ export default function ProfileReview({ reviewData }) {
                     </div>
                     {/* <p className="w-full text-sm">{review_text}</p> */}
                     {/* show review and blocked, show review and */}
-                    {showReviewText ? 
-                        isBlockedUser ? 
-                        <div/> 
-                        :
-                        <ReviewText className="w-full text-sm" content={review_text}/> 
-                    : <div/> }
+                    {showReviewText == true ?
+                        <ReviewText className="max-w-80 text-sm pt-2 ml-1 text-container" content={displayText} />
+                        : <div />}
                     
                     
             </div>    
